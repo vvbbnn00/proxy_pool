69926da761b61b6409c80b5b5db41a3b5762cf84
vvbbnn00 <vvbbnn00@foxmail.com>
2024-01-20 20:24:06 +0800

    修改最新可用的公开API配置，针对cloudflare进行绕过

--- fetcher/proxyFetcher.py
+++ fetcher/proxyFetcher.py
@@ -13,9 +13,9 @@
 __author__ = 'JHao'
 
 import re
-import json
 from time import sleep
 
+from util.proxyBypassRequest import ProxyBypassRequest
 from util.webRequest import WebRequest
 
 
@@ -34,7 +34,7 @@ class ProxyFetcher(object):
         latest_page_time = html_tree.xpath("//span[@class='thread_time_info']/text()")[0].strip()
         from datetime import datetime
         interval = datetime.now() - datetime.strptime(latest_page_time, "%Y/%m/%d %H:%M:%S")
-        if interval.seconds < 300:  # 只采集5分钟内的更新
+        if interval.seconds < 24 * 3600:  # 只采集24小时内的代理
             target_url = "https://www.zdaye.com/" + html_tree.xpath("//h3[@class='thread_title']/a/@href")[0].strip()
             while target_url:
                 _tree = WebRequest().get(target_url, verify=False).tree
@@ -49,15 +49,16 @@ class ProxyFetcher(object):
     @staticmethod
     def freeProxy02():
         """
-        代理66 http://www.66ip.cn/
+        69ip http://www.69ip.cn/
         """
-        url = "http://www.66ip.cn/"
-        resp = WebRequest().get(url, timeout=10).tree
-        for i, tr in enumerate(resp.xpath("(//table)[3]//tr")):
-            if i > 0:
-                ip = "".join(tr.xpath("./td[1]/text()")).strip()
-                port = "".join(tr.xpath("./td[2]/text()")).strip()
-                yield "%s:%s" % (ip, port)
+        url = "http://www.69ip.cn/?page=1"
+        try:
+            tree = WebRequest().get(url, timeout=30).tree
+            proxy_list = tree.xpath('.//table//tr')
+            for tr in proxy_list[1:]:
+                yield ':'.join(tr.xpath('./td/text()')[0:2])
+        except Exception as e:
+            print(e)
 
     @staticmethod
     def freeProxy03():
@@ -72,21 +73,13 @@ class ProxyFetcher(object):
 
     @staticmethod
     def freeProxy04():
-        """ FreeProxyList https://www.freeproxylists.net/zh/ """
-        url = "https://www.freeproxylists.net/zh/?c=CN&pt=&pr=&a%5B%5D=0&a%5B%5D=1&a%5B%5D=2&u=50"
-        tree = WebRequest().get(url, verify=False).tree
-        from urllib import parse
-
-        def parse_ip(input_str):
-            html_str = parse.unquote(input_str)
-            ips = re.findall(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', html_str)
-            return ips[0] if ips else None
-
-        for tr in tree.xpath("//tr[@class='Odd']") + tree.xpath("//tr[@class='Even']"):
-            ip = parse_ip("".join(tr.xpath('./td[1]/script/text()')).strip())
-            port = "".join(tr.xpath('./td[2]/text()')).strip()
-            if ip:
-                yield "%s:%s" % (ip, port)
+        urls = ['https://list.proxylistplus.com/Fresh-HTTP-Proxy-List-1']
+        request = WebRequest()
+        for url in urls:
+            r = request.get(url, timeout=20)
+            proxies = re.findall(r'<td>(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})</td>[\s\S]*?<td>(\d+)</td>', r.text)
+            for proxy in proxies:
+                yield ':'.join(proxy)
 
     @staticmethod
     def freeProxy05(page_count=1):
@@ -134,21 +127,25 @@ class ProxyFetcher(object):
         """ 小幻代理 """
         urls = ['https://ip.ihuan.me/address/5Lit5Zu9.html']
         for url in urls:
-            r = WebRequest().get(url, timeout=10)
+            r = ProxyBypassRequest().get(url, timeout=60)
             proxies = re.findall(r'>\s*?(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s*?</a></td><td>(\d+)</td>', r.text)
             for proxy in proxies:
                 yield ":".join(proxy)
 
     @staticmethod
-    def freeProxy09(page_count=1):
-        """ 免费代理库 """
-        for i in range(1, page_count + 1):
-            url = 'http://ip.jiangxianli.com/?country=中国&page={}'.format(i)
-            html_tree = WebRequest().get(url, verify=False).tree
-            for index, tr in enumerate(html_tree.xpath("//table//tr")):
-                if index == 0:
-                    continue
-                yield ":".join(tr.xpath("./td/text()")[0:2]).strip()
+    def freeProxy09():
+        """
+        https://proxy-list.org/english/index.php
+        :return:
+        """
+        urls = ['https://proxy-list.org/english/index.php?p=%s' % n for n in range(1, 10)]
+        request = WebRequest()
+        import base64
+        for url in urls:
+            r = request.get(url, timeout=10)
+            proxies = re.findall(r"Proxy\('(.*?)'\)", r.text)
+            for proxy in proxies:
+                yield base64.b64decode(proxy).decode()
 
     @staticmethod
     def freeProxy10():
@@ -235,7 +232,5 @@ class ProxyFetcher(object):
 
 if __name__ == '__main__':
     p = ProxyFetcher()
-    for _ in p.freeProxy06():
+    for _ in p.freeProxy08():
         print(_)
-
-# http://nntime.com/proxy-list-01.htm
---
+++ util/proxyBypassRequest.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     ProxyBypassRequest
+   Description :   Use FlareSolverr to bypass cloudflare
+   Author :        vvbbnn00
+   date：          2024/1/20
+-------------------------------------------------
+"""
+__author__ = 'vvbbnn00'
+
+import json
+
+from lxml import etree
+import requests
+import time
+
+from handler.logHandler import LogHandler
+
+requests.packages.urllib3.disable_warnings()
+
+
+class ResponseCustom(object):
+    status_code = 200
+    text = ""
+    content = ""
+
+
+class ProxyBypassRequest(object):
+    FlareSolverrUrl = "http://flaresolverr:8191/v1"
+    name = "proxy_bypass_request"
+
+    def __init__(self, *args, **kwargs):
+        self.log = LogHandler(self.name, file=False)
+        self.response = ResponseCustom()
+
+    def get(self, url, retry_time=3, retry_interval=5, timeout=5, *args, **kwargs):
+        """
+        get method
+        :param url: target url
+        :param retry_time: retry time
+        :param retry_interval: retry interval
+        :param timeout: network timeout
+        :return:
+        """
+        while True:
+            try:
+                url = self.FlareSolverrUrl
+                headers = {"Content-Type": "application/json"}
+                data = {
+                    "cmd": "request.get",
+                    "url": "https://ip.ihuan.me/address/5Lit5Zu9.html",
+                    "maxTimeout": timeout * 1000
+                }
+                response = requests.post(url, headers=headers, json=data).json()
+                if response["status"] != "ok":
+                    raise Exception(response["message"])
+                solution = response["solution"]
+                self.response.status_code = solution["status"]
+                self.response.text = solution["response"]
+                self.response.content = solution["response"].encode("utf-8")
+                return self
+            except Exception as e:
+                self.log.error("requests: %s error: %s" % (url, str(e)))
+                retry_time -= 1
+                if retry_time <= 0:
+                    resp = ResponseCustom()
+                    resp.status_code = 200
+                    return self
+                self.log.info("retry %s second after" % retry_interval)
+                time.sleep(retry_interval)
+
+    @property
+    def tree(self):
+        return etree.HTML(self.response.content)
+
+    @property
+    def text(self):
+        return self.response.text
+
+    @property
+    def json(self):
+        try:
+            return json.loads(self.response.text)
+        except Exception as e:
+            self.log.error(str(e))
+            return {}
